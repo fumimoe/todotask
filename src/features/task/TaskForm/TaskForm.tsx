2f8ea2c55a1e708ab4ffc3f46414904e9b902749
押切郁也 <fumi.topone@gmail.com>
2021-03-19 23:49:42 +0900

    create modal

--- src/features/task/TaskForm/TaskForm.tsx
+++ src/features/task/TaskForm/TaskForm.tsx
@@ -2,38 +2,61 @@ import React from "react";
 import styles from "./TaskForm.module.scss";
 import TextField from "@material-ui/core/TextField";
 import { useForm } from "react-hook-form";
-import {createTask} from '../TaskSlice';
-import {useDispatch} from 'react-redux';
-
+import { createTask } from "../TaskSlice";
+import { useDispatch } from "react-redux";
 
 type Inputs = {
   taskTitle: string;
 };
 
-const TaskForm: React.FC = () => {
+type PropTypes = {
+  // ?とすることで必ずしも渡されなくてもいいよを宣言する
+  edit?: boolean;
+};
+
+const TaskForm: React.FC<PropTypes> = ({ edit }) => {
   const dispatch = useDispatch();
   const { register, handleSubmit, reset } = useForm();
   const handleCreate = (data: Inputs) => {
     // dataはregisterで登録したdataが入ってくる
     // dispatchすることでcreateTaskが発火する。引数にTextFieldで入力した値のnameを渡す
     // data.taskTitleをaction.payloadに渡すことができる
-    dispatch(createTask(data.taskTitle))
+    dispatch(createTask(data.taskTitle));
     reset();
   };
+
+  const handleEdit = (data:Inputs) => {
+    console.log(data);
+  }
   return (
     <div className={styles.root}>
-        {/* handlesubmitはsubmitされた時に実行される関数 */}
-      <form onSubmit={handleSubmit(handleCreate)} className={styles.form} noValidate autoComplete="off">
+      {/* handlesubmitはsubmitされた時に実行される関数 */}
+      <form
+      
+        onSubmit={edit ?handleSubmit(handleEdit): handleSubmit(handleCreate)}
+        className={styles.form}
+        noValidate
+        autoComplete="off"
+      >
         <TextField
+        // 編集ボタンを押した時にtitleの名前が表示される
+        defaultValue={edit? 'defaultValue' : ""}
           id="outlined-basic"
-          label="NewTask"
+          // ここで条件分岐を行ってる
+          label={edit ? "edit task" : "new task"}
           variant="outlined"
           className={styles.text_field}
-        //   どこからデータを取得するのか→今回はfrom からデータを取得する
+          //   どこからデータを取得するのか→今回はfrom からデータを取得する
           inputRef={register}
-        //   どういったdataの名前で渡すかを記述する
-          name='taskTitle'
+          //   どういったdataの名前で渡すかを記述する
+          name="taskTitle"
         />
+        {edit ? (
+          <div className={styles.button_wrapper}>
+            <button type="submit" className={styles.submit_button} >変更</button>
+            <button type="button" className={styles.cancel_button}>キャンセル</button>
+          </div>
+        ) : null}
       </form>
     </div>
   );
--- src/features/task/TaskItem/TaskItem.tsx
+++ src/features/task/TaskItem/TaskItem.tsx
@@ -5,12 +5,26 @@ import EventNoteIcon from "@material-ui/icons/EventNote";
 import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
 import IconButton from "@material-ui/core/IconButton";
+import Modal from "@material-ui/core/Modal";
+import TaskForm from "../TaskForm/TaskForm";
+
 
 interface PropTypes {
   task: { id: number; title: string; completed: boolean };
 }
 
 const TaskItem: React.FC<PropTypes> = ({ task }) => {
+  const [open, setOpen] = React.useState(false);
+
+  // モーダルの開閉関数
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className={styles.root}>
       <div className={styles.title}>
@@ -19,30 +33,43 @@ const TaskItem: React.FC<PropTypes> = ({ task }) => {
           {/* propsで受け取る */}
           {task.title}
         </div>
-        </div>
+      </div>
 
-        <div className={styles.right_item}>
-          <IconButton>
+      <div className={styles.right_item}>
+        <IconButton>
           <Checkbox
             checked={task.completed}
             onClick={() => console.log("checkbox")}
             className={styles.icon}
           />
-          </IconButton>
-          <IconButton
-            onClick={() => console.log("edit")}
-            className={styles.edit_button}
-          >
-            <EditIcon className={styles.icon} />
-          </IconButton>
-          <IconButton
-            onClick={() => console.log("delete")}
-            className={styles.delete_button}
-          >
-            <DeleteIcon className={styles.icon} />
-          </IconButton>
+        </IconButton>
+        <IconButton
+          className={styles.edit_button}
+          onClick={handleOpen}
+        >
+          <EditIcon className={styles.icon} />
+        </IconButton>
+        <IconButton
+          onClick={() => console.log("delete")}
+          className={styles.delete_button}
+        >
+          <DeleteIcon className={styles.icon} />
+        </IconButton>
+      </div>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        className={styles.modal}
+      >
+        <div className={styles.modal_content}>
+          <div className="styles.modal_start">
+            edit task
+            {/* 条件分岐を行うためにTaskFormにeditを送ってる */}
+            <TaskForm edit/>
+          </div>
         </div>
-      
+      </Modal>
     </div>
   );
 };
